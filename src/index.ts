28081b318838b54d575c91e50bc04c59daa005d3
Nicholas Chung <iamnicholaschung@gmail.com>
2021-06-16 10:54:04 +0800

    Update index

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
+require("dotenv");
 const connectDB = require("./utils/db");
+
 const app = require("./app");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000 || 3001;
 
 connectDB();
 
-const server = app.listen(process.env.PORT || PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Started Express app on http://localhost:${PORT}`);
 });
