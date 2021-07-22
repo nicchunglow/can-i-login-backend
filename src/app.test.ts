5dbbb37d0957174d581638e7020a9950ae604304
nicchunglow <iamnicholaschung@gmail.com>
2021-07-23 00:18:59 +0800

    update test

--- src/app.test.ts
+++ src/app.test.ts
@@ -9,13 +9,15 @@ mongoose.set("useCreateIndex", true);
 mongoose.set("useUnifiedTopology", true);
 
 describe("/", () => {
-  it("should contain the home directory", async () => {
-    const expectedDirectory = {
-      0: "GET   /",
-      "1": "POST /users/register",
-      "2": "POST /users/login",
-    };
-    const { body: response } = await request(app).get("/");
-    expect(response).toEqual(expectedDirectory);
-  });
+	it("should contain the home directory", async () => {
+		const expectedDirectory = {
+			0: "GET   /",
+			"1": "POST /users/register",
+			"2": "POST /users/login",
+			"3": "GET /reports",
+			"4": "GET /auth",
+		};
+		const { body: response } = await request(app).get("/");
+		expect(response).toEqual(expectedDirectory);
+	});
 });
